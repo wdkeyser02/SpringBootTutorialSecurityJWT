dba53f6c47b652f9d4c93587e5d318f889e4fb10
willydekeyser <willy.de.keyser@skynet.be>
2022-12-19 21:16:16 +0100

    Setup Spring Security JWT

--- SpringBootSecurityJWT/src/main/java/willydekeyser/config/SecurityConfig.java
+++ SpringBootSecurityJWT/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -1,25 +1,41 @@
 package willydekeyser.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.security.config.Customizer.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import willydekeyser.jose.Jwks;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private RSAKey rsaKey;
+	
 	@Bean
 	InMemoryUserDetailsManager user() {
 		return new InMemoryUserDetailsManager(
 				User.withUsername("User")
 				.password("{noop}password")
-				.authorities("ROLE_USER")
+				.authorities("read")
 				.build());
 	}
 	
@@ -29,10 +45,29 @@ public class SecurityConfig {
 				.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(
 						auth -> auth
+						.requestMatchers("/token").permitAll()
 						.anyRequest().authenticated())
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
 				.sessionManagement(session -> session
 						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.httpBasic(withDefaults())
 				.build();
 	}
+	
+	@Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        rsaKey = Jwks.generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwks) {
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() throws JOSEException {
+         return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+    }
 }
