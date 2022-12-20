b997093cc957cc21fcbc08f7e335e524006516d1
willydekeyser <willy.de.keyser@skynet.be>
2022-12-20 09:24:05 +0100

    Update Controller

--- SpringBootSecurityJWT/src/main/java/willydekeyser/controller/AuthController.java
+++ SpringBootSecurityJWT/src/main/java/willydekeyser/controller/AuthController.java
@@ -1,14 +1,17 @@
 package willydekeyser.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.classic.Logger;
 import willydekeyser.service.TokenService;
 
 @RestController
 public class AuthController {
 
+	private static final Logger LOG = (Logger) LoggerFactory.getLogger(AuthController.class);
 	private final TokenService tokenService;
 	
     public AuthController(TokenService tokenService) {
@@ -17,7 +20,9 @@ public class AuthController {
 
 	@PostMapping("/token")
     public String token(Authentication authentication) {
+		LOG.debug("Token request for user: '{}'", authentication.getName());
 		String token = tokenService.generateToken(authentication);
+		LOG.debug("Token: {}", token);
 		return token;
 	}
 
